5759eb6ba86c36c4e8bb288eb050978cac797f45
euvald <l21141286@queretaro.tecnm.mx>
2024-09-26 14:24:36 -0600

    Add files via upload

---
+++ Materias.js
@@ -0,0 +1,74 @@
+misDatos({
+    "materias":[
+        {
+            "Clave":"ACA0907",
+            "Nombre":"Taller de Ética",
+            "Creditos":4,
+            "Carrera":"Ing. en Sist. Comp.",
+            "horario":"Lunes de 8:00 am a 10:am"
+        },
+        {
+            "Clave":"ACA0901",
+            "Nombre":"Cálculo Diferencial",
+            "Creditos":5,
+            "Carrera":"Ing. en Sist. Comp.",
+            "horario":"Lunes de 10:00 am a 12:00 pm"
+        },
+        {
+            "Clave":"AEF1041",
+            "Nombre":"Matemáticas Discretas",
+            "Creditos":5,
+            "Carrera":"Ing. en Sist. Comp.",
+            "horario":"Lunes de 12:00 pm a 13:00 pm"
+        },
+        {
+            "Clave":"ACC0906",
+            "Nombre":"Fundamentos de Investigación",
+            "Creditos":4,
+            "Carrera":"Ing. en Sist. Comp.",
+            "horario":"Lunes de 13:00 pm a 14:00 pm"
+        },
+        {
+            "Clave":"AED1285",
+            "Nombre":"Fundamentos de Programación",
+            "Creditos":5,
+            "Carrera":"Ing. en Sist. Comp.",
+            "horario":"Lunes de 14:00 pm a 15:00 pm"
+        },
+        {
+            "Clave":"SCH1024",
+            "Nombre":"Taller de Administración",
+            "Creditos":4,
+            "Carrera":"Ing. en Sist. Comp.",
+            "horario":"Lunes de 13:00 pm a 16:00 pm"
+        },
+        {
+            "Clave":"ACF0902",
+            "Nombre":"Cálculo Integral",
+            "Creditos":5,
+            "Carrera":"Ing. industrial",
+            "horario":"Lunes de 8:00 am a 10:00 am"
+        },
+            {
+            "Clave":"AEC1008",
+            "Nombre":"Contabilidad Financiera",
+            "Creditos":4,
+            "Carrera":"Ing. industrial",
+            "horario":"Lunes de 10:00 am a 11:00 am"
+        },
+        {
+            "Clave":"AEC1058",
+            "Nombre":"Química",
+            "Creditos":4,
+            "Carrera":"Ing. industrial",
+            "horario":"Lunes de 12:00 pm a 15:00 pm"
+        },
+            {
+            "Clave":"SCD1018",
+            "Nombre":"Principios Eléctricos y Aplicaciones Digitales",
+            "Creditos":4,
+            "Carrera":"Ing. industrial",
+            "horario":"Lunes de 15:00 pm a 16:00 pm"    
+        },
+    ]
+});
